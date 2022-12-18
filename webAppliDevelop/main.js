ab1439f0a50eb3972895b9106866cb92e95d9431
nosenami <n-nose@esm.co.jp>
2022-12-18 18:59:28 +0900

    Webアプリ開発4-3 新規作成

--- webAppliDevelop/main.js
+++ webAppliDevelop/main.js
@@ -31,6 +31,70 @@ function saveToSpreadSheet(count) {
 
 
 /**
+ * テスト用の呼び出し関数。
+ * 今後ボタンによる動作を実装したら当関数は削除する。
+ */
+function testMain(){
+  console.log( updateToDatabase(12345) );
+}
+
+
+/** 接続名（インスタンス名） */
+const CONNECTION_NAME = 'esm-gcp-study:us-central1:modern-study';
+/** データベース名 */
+const DATABASE_NAME = 'web_book_nosenami';
+/** データベースのＵＲＬ */
+const DATABASE_URL = 'jdbc:google:mysql://' + CONNECTION_NAME + '/' + DATABASE_NAME;
+
+/**ユーザーＩＤ */
+const USER_NAME = 'esm';
+/**パスワード  */
+const PASSWORD = 'esm';
+
+/** ＳＱＬ文（counter_tableのcountをＵＰＤＡＴＥする。） */
+const SQL_STATEMENT_UPDATE =
+  "UPDATE  " +
+  "  web_book_nosenami.counter_table " +
+  "SET  " +
+  "  count = ?  " +
+  "WHERE  " +
+  "  counter_id = 1  ";
+
+
+/**
+ * ＤＢへの更新を行う。
+ * @param {*} count カウンタ値。（counter_tableのcountをこの値で更新する。）
+ * @returns 更新結果の行数である１。
+ */
+function updateToDatabase(count) {
+
+  //ＵＰＤＡＴＥ実行の手順①
+  //ＤＢへ接続し、JdbcConnectionというオブジェクトを受け取る。
+  //  JDBCとは、接続に関するサービス。
+  const jdbcConnection = Jdbc.getCloudSqlConnection(DATABASE_URL, USER_NAME, PASSWORD);
+
+  //ＵＰＤＡＴＥ実行の手順②
+  //JdbcConnectionというオブジェクトに対してＳＱＬ文を設定し、
+  //JdbcPreparedStatementというオブジェクトを受け取る。
+  const jdbcPreparedStatement = jdbcConnection.prepareStatement(SQL_STATEMENT_UPDATE);
+
+  //ＵＰＤＡＴＥ実行の手順③
+  //JdbcPreparedStatementというオブジェクトに対して
+  //ＳＱＬ文の「？」パラメタ値を設定して実行する。
+  //更新された行数を受け取る。
+  jdbcPreparedStatement.setInt(1,count);
+  const resultRows = jdbcPreparedStatement.executeUpdate();
+
+  //ＵＰＤＡＴＥ実行の手順④
+  //接続を終了する。
+  jdbcPreparedStatement.close();
+  jdbcConnection.close();
+
+  return resultRows;
+}
+
+
+/**
  * スプレッドシートの値を取得し返却する。
  * @returns スプレッドシートのA1セルの値。（カウンターの値。）
  */
