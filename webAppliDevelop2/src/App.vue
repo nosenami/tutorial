b0b472c7792d37ee924a0ecaf52d47a7d5ca93b6
nosenami <n-nose@esm.co.jp>
2023-04-16 19:06:05 +0900

    プロジェクト土台をクローンし、動作確認できた。

---
+++ webAppliDevelop2/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app id="app-root">
+    <v-app-bar
+      app
+      dark
+      color="primary"
+    >
+      <v-toolbar-title>
+        書籍管理アプリ
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <book-app />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+import BookApp from '@/view/BookApp'
+
+export default Vue.extend({
+  components: {
+    BookApp
+  },
+  data () {
+    return {}
+  },
+  created () {}
+})
+</script>
